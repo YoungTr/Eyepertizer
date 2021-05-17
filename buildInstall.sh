429937353a1d36d81ba95ed6d66dddc3b3894292
YoungTr <tangyan1004@gmail.com>
2021-05-17 21:54:18 +0800

    sh:start splash activity

--- buildInstall.sh
+++ buildInstall.sh
@@ -5,4 +5,4 @@ echo "------------------build finish------------------"
 echo "------------------start install------------------"
 adb install -r ./app/build/outputs/apk/debug/app-debug.apk
 echo "------------------start app------------------"
-adb shell am start -n com.eyepertizer.androidx/com.eyepertizer.androidx.MainActivity
+adb shell am start -n com.eyepertizer.androidx/com.eyepertizer.androidx.ui.splash.view.SplashActivity
